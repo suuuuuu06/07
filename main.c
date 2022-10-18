866d2f9bb2d55c67f0c5c84a0969d965ce314e95
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-10-18 16:29:29 +0900

    2

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 int main(void)
 {
-	int flag=1;
-	while(flag!=0)
+	int i;
+	
+	for (i=0;i<5;i++)
 	{
-		int y;
-		y=3;
-		flag=0;
+		int temp =1;
+		printf("temp=%d\n", temp);
+		temp++;
 	}
-	
-	//y=4;
-	
+	return 0;
 }
